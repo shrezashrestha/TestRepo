a72c587e56ce333c9fb18fed2855325591baa11c
shrezashrestha <shrezashrestha22@augustana.edu>
2024-09-30 00:37:34 -0500

    Now with dancing chicken

---
+++ SillyBirdProject/src/main/java/edu/augustana/Chicken.java
@@ -0,0 +1,10 @@
+package edu.augustana;
+
+public class Chicken {
+
+    public void dance(){
+
+        System.out.println("Chicken dance!");
+    }
+}
+
--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -3,5 +3,11 @@ package edu.augustana;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
+
+        Chicken javaChicken = new Chicken();
+        javaChicken.dance();
     }
+
+
+
 }
